6c40effba41a55bf906e82b546441112dd48d3f4
JooHyung <jhp21c@naver.com>
2023-05-10 19:55:22 +0900

    [Feat]

--- JooHyung/faster_rcnn/run.sh
+++ JooHyung/faster_rcnn/run.sh
@@ -1,3 +1,3 @@
-cd /opt/ml/detection/baseline/mmdetection/
-python tools/train.py ../../../_boost_/youngun/configs/faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco.py \
+cd /Desktop/AISM_MMDETECTION/
+python tools/train.py ./JooHyung/faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco.py \
 --work-dir ./outputs/faster-rcnn-resneXt-higherLr-512
